918f37b1ed9c28200cbfe4d03130b9e21cfaa336
Tamal Saha <tamal@appscode.com>
2020-04-24 00:12:17 -0700

    Move apireg annotation to operator pod (#64)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- apis/installer/v1alpha1/openapi_generated.go
+++ apis/installer/v1alpha1/openapi_generated.go
@@ -15949,6 +15949,20 @@ func schema_installer_apis_installer_v1alpha1_KubeDBOperatorSpec(ref common.Refe
 							},
 						},
 					},
+					"podAnnotations": {
+						SchemaProps: spec.SchemaProps{
+							Type: []string{"object"},
+							AdditionalProperties: &spec.SchemaOrBool{
+								Allows: true,
+								Schema: &spec.Schema{
+									SchemaProps: spec.SchemaProps{
+										Type:   []string{"string"},
+										Format: "",
+									},
+								},
+							},
+						},
+					},
 					"nodeSelector": {
 						SchemaProps: spec.SchemaProps{
 							Type: []string{"object"},
