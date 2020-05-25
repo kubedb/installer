8d8f75d8d6e9ea7890378615e3789ae820e085ea
Tamal Saha <tamal@appscode.com>
2020-05-25 09:54:48 -0700

    Generate both v1beta1 and v1 CRD YAML (#80)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

---
+++ vendor/kmodules.xyz/client-go/meta/hash.go
@@ -0,0 +1,155 @@
+/*
+Copyright The Kmodules Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package meta
+
+import (
+	"fmt"
+	"hash"
+	"hash/fnv"
+	"reflect"
+	"strconv"
+
+	"github.com/appscode/go/encoding/json/types"
+	"github.com/davecgh/go-spew/spew"
+	"github.com/fatih/structs"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
+// ObjectHash includes all top label fields (like data, spec) except TypeMeta, ObjectMeta and Status
+// also includes Generation, Annotation and Labels form ObjectMeta
+func ObjectHash(in metav1.Object) string {
+	obj := make(map[string]interface{})
+
+	obj["generation"] = in.GetGeneration()
+	if len(in.GetLabels()) > 0 {
+		obj["labels"] = in.GetLabels()
+	}
+
+	if len(in.GetAnnotations()) > 0 {
+		data := make(map[string]string, len(in.GetAnnotations()))
+		for k, v := range in.GetAnnotations() {
+			if k != LastAppliedConfigAnnotation {
+				data[k] = v
+			}
+		}
+		obj["annotations"] = data
+	}
+
+	st := structs.New(in)
+	for _, field := range st.Fields() {
+		fieldName := field.Name()
+		if fieldName != "ObjectMeta" && fieldName != "TypeMeta" && fieldName != "Status" {
+			obj[fieldName] = field.Value()
+		}
+	}
+
+	h := fnv.New64a()
+	DeepHashObject(h, obj)
+	return strconv.FormatUint(h.Sum64(), 10)
+}
+
+func GenerationHash(in metav1.Object) string {
+	obj := make(map[string]interface{}, 3)
+	obj["generation"] = in.GetGeneration()
+	if len(in.GetLabels()) > 0 {
+		obj["labels"] = in.GetLabels()
+	}
+	if len(in.GetAnnotations()) > 0 {
+		data := make(map[string]string, len(in.GetAnnotations()))
+		for k, v := range in.GetAnnotations() {
+			if k != LastAppliedConfigAnnotation {
+				data[k] = v
+			}
+		}
+		obj["annotations"] = data
+	}
+	h := fnv.New64a()
+	DeepHashObject(h, obj)
+	return strconv.FormatUint(h.Sum64(), 10)
+}
+
+// DeepHashObject writes specified object to hash using the spew library
+// which follows pointers and prints actual values of the nested objects
+// ensuring the hash does not change when a pointer changes.
+func DeepHashObject(hasher hash.Hash, objectToWrite interface{}) {
+	hasher.Reset()
+	printer := spew.ConfigState{
+		Indent:         " ",
+		SortKeys:       true,
+		DisableMethods: true,
+		SpewKeys:       true,
+	}
+	printer.Fprintf(hasher, "%#v", objectToWrite)
+}
+
+func MustAlreadyReconciled(o interface{}) bool {
+	reconciled, err := AlreadyReconciled(o)
+	if err != nil {
+		panic("failed to extract status.observedGeneration field due to err:" + err.Error())
+	}
+	return reconciled
+}
+
+func AlreadyReconciled(o interface{}) (bool, error) {
+	var generation, observedGeneration *types.IntHash
+	var err error
+
+	switch obj := o.(type) {
+	case *unstructured.Unstructured:
+		generation, observedGeneration, err = extractGenerationFromUnstructured(obj)
+	case metav1.Object:
+		generation, observedGeneration, err = extractGenerationFromObject(obj)
+	default:
+		err = fmt.Errorf("unknown object type %s", reflect.TypeOf(o).String())
+	}
+	if err != nil {
+		return false, err
+	}
+	return observedGeneration.MatchGeneration(generation), nil
+}
+
+func extractGenerationFromUnstructured(obj *unstructured.Unstructured) (*types.IntHash, *types.IntHash, error) {
+	generation := types.IntHashForGeneration(obj.GetGeneration())
+
+	val, found, err := unstructured.NestedFieldNoCopy(obj.Object, "status", "observedGeneration")
+	if err != nil {
+		return nil, nil, err
+	} else if !found {
+		return nil, nil, fmt.Errorf("status.observedGeneration is missing")
+	}
+	observedGeneration, err := types.ParseIntHash(val)
+
+	return generation, observedGeneration, err
+}
+
+func extractGenerationFromObject(obj metav1.Object) (*types.IntHash, *types.IntHash, error) {
+	generation := types.IntHashForGeneration(obj.GetGeneration())
+
+	st := structs.New(obj)
+	fieldStatus, found := st.FieldOk("Status")
+	if !found {
+		return nil, nil, fmt.Errorf("status is missing")
+	}
+	fieldObsGen, found := fieldStatus.FieldOk("ObservedGeneration")
+	if !found {
+		return nil, nil, fmt.Errorf("status.observedGeneration is missing")
+	}
+	observedGeneration, err := types.ParseIntHash(fieldObsGen.Value())
+
+	return generation, observedGeneration, err
+}
---
+++ vendor/kmodules.xyz/client-go/meta/incluster.go
@@ -0,0 +1,90 @@
+/*
+Copyright The Kmodules Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package meta
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	core "k8s.io/api/core/v1"
+	"k8s.io/client-go/rest"
+)
+
+func Namespace() string {
+	if ns := os.Getenv("KUBE_NAMESPACE"); ns != "" {
+		return ns
+	}
+	if data, err := ioutil.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/namespace"); err == nil {
+		if ns := strings.TrimSpace(string(data)); len(ns) > 0 {
+			return ns
+		}
+	}
+	return core.NamespaceDefault
+}
+
+// PossiblyInCluster returns true if loading an inside-kubernetes-cluster is possible.
+func PossiblyInCluster() bool {
+	fi, err := os.Stat("/var/run/secrets/kubernetes.io/serviceaccount/token")
+	return os.Getenv("KUBERNETES_SERVICE_HOST") != "" &&
+		os.Getenv("KUBERNETES_SERVICE_PORT") != "" &&
+		err == nil && !fi.IsDir()
+}
+
+func APIServerCertificate(cfg *rest.Config) (*x509.Certificate, error) {
+	err := rest.LoadTLSFiles(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	// create ca cert pool
+	caCertPool := x509.NewCertPool()
+	ok := caCertPool.AppendCertsFromPEM(cfg.CAData)
+	if !ok {
+		return nil, fmt.Errorf("can't append caCert to caCertPool")
+	}
+
+	tr := &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+			DualStack: true,
+		}).DialContext,
+		MaxIdleConns:          100,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+		TLSClientConfig:       &tls.Config{RootCAs: caCertPool},
+	}
+	client := &http.Client{Transport: tr}
+
+	resp, err := client.Get(cfg.Host)
+	if err != nil {
+		return nil, err
+	}
+	for i := range resp.TLS.VerifiedChains {
+		return resp.TLS.VerifiedChains[i][0], nil
+	}
+	return nil, fmt.Errorf("no cert found")
+}
