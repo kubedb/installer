283233be0acd2c0e60d7216206ac283aa46d2e11
Tamal Saha <tamal@appscode.com>
2022-05-13 10:29:29 -0700

    Update registry templates to support custom default registry (ghcr.io) (#490)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- hack/fmt/main.go
+++ hack/fmt/main.go
@@ -46,7 +46,6 @@ import (
 )
 
 const (
-	registryKubeDB = "kubedb"
 	distroOfficial = "Official"
 )
 
@@ -209,8 +208,8 @@ func main() {
 						distro = "Percona"
 					}
 					if img, ok, _ := unstructured.NestedString(ri.Object.UnstructuredContent(), "spec", "db", "image"); ok {
-						reg, _, _ := ParseImage(img)
-						if reg == "mysql" {
+						_, repo, _, _ := ParseImage(img)
+						if repo == "mysql" {
 							distro = "MySQL"
 						}
 					}
@@ -495,21 +494,22 @@ func main() {
 					templatizeRegistry := func(field string) {
 						img, ok, _ := unstructured.NestedString(obj.Object, "spec", prop, field)
 						if ok {
-							reg, bin, tag := ParseImage(img)
+							reg, repo, bin, tag := ParseImage(img)
 							var newimg string
 							switch {
-							case tag == "" && reg == "":
-								newimg = fmt.Sprintf(`{{ include "official.registry" (set (.Values | deepCopy) "officialRegistry" (list %q)) }}`, bin)
-							case tag == "" && reg == registryKubeDB:
-								newimg = fmt.Sprintf(`{{ include "catalog.registry" . }}/%s`, bin)
-							case tag == "" && reg != registryKubeDB:
-								newimg = fmt.Sprintf(`{{ include "official.registry" (set (.Values | deepCopy) "officialRegistry" (list %q %q)) }}`, reg, bin)
-							case tag != "" && reg == "":
-								newimg = fmt.Sprintf(`{{ include "official.registry" (set (.Values | deepCopy) "officialRegistry" (list %q)) }}:%s`, bin, tag)
-							case reg == registryKubeDB:
-								newimg = fmt.Sprintf(`{{ include "catalog.registry" . }}/%s:%s`, bin, tag)
+							case tag == "" && (reg != "" || repo != ""):
+								newimg = fmt.Sprintf(`{{ include "catalog.registry" (merge (dict "_reg" "%s" "_repo" "%s") .Values) }}/%s`, reg, repo, bin)
+							case tag != "" && (reg != "" || repo != ""):
+								newimg = fmt.Sprintf(`{{ include "catalog.registry" (merge (dict "_reg" "%s" "_repo" "%s") .Values) }}/%s:%s`, reg, repo, bin, tag)
+							case tag == "":
+								newimg = fmt.Sprintf(`{{ include "official.registry" (merge (dict "_bin" "%s") .Values) }}`, bin)
 							default:
-								newimg = fmt.Sprintf(`{{ include "official.registry" (set (.Values | deepCopy) "officialRegistry" (list %q %q)) }}:%s`, reg, bin, tag)
+								newimg = fmt.Sprintf(`{{ include "official.registry" (merge (dict "_bin" "%s") .Values) }}:%s`, bin, tag)
+
+								// case tag == "":
+								//	newimg = fmt.Sprintf(`{{ include "official.registry" (set (.Values | deepCopy) "officialRegistry" (list %q)) }}`, bin)
+								// default:
+								//	newimg = fmt.Sprintf(`{{ include "official.registry" (set (.Values | deepCopy) "officialRegistry" (list %q)) }}:%s`, bin, tag)
 							}
 							err = unstructured.SetNestedField(obj.Object, newimg, "spec", prop, field)
 							if err != nil {
@@ -727,17 +727,23 @@ func main() {
 	}
 }
 
-func ParseImage(s string) (reg, bin, tag string) {
+func ParseImage(s string) (reg, repo, bin, tag string) {
 	idx := strings.IndexRune(s, ':')
 	if idx != -1 {
 		tag = s[idx+1:]
 		s = s[:idx]
 	}
-	idx = strings.IndexRune(s, '/')
-	if idx == -1 {
-		bin = s
-	} else {
-		reg, bin = s[:idx], s[idx+1:]
+	parts := strings.Split(s, "/")
+	if len(parts) >= 1 {
+		bin = parts[len(parts)-1]
+		parts = parts[:len(parts)-1]
+	}
+	if len(parts) >= 1 {
+		repo = parts[len(parts)-1]
+		parts = parts[:len(parts)-1]
+	}
+	if len(parts) > 0 {
+		reg = strings.Join(parts, "/")
 	}
 	return
 }
