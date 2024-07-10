fe0195fd789291ce1940c65b236782e87f5624ab
Anisur Rahman <anisur@appscode.com>
2024-07-10 16:02:27 +0600

    Init MSSQLServer archiver backup
    Signed-off-by: Anisur Rahman <anisur@appscode.com>

--- hack/scripts/import-crds.sh
+++ hack/scripts/import-crds.sh
@@ -20,7 +20,7 @@ crd_dir=${1:-}/apimachinery/crds
 update_kubedb_crds=true
 
 api_repo_url=https://github.com/kubedb/apimachinery.git
-api_repo_tag=${KUBEDB_APIMACHINERY_TAG:-mssql}
+api_repo_tag=${KUBEDB_APIMACHINERY_TAG:-mssqlserver-archiver}
 
 if [ "$#" -ne 1 ]; then
     if [ "${api_repo_tag}" == "master" ]; then
