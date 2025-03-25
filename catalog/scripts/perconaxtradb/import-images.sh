a4bc91d0213d3efc1af1a6618b957de2de67ecc0
1gtm <1gtm@appscode.com>
2025-03-25 00:10:32 +0000

    Prepare for release v2025.3.24
    ProductLine: KubeDB
    
    Release: v2025.3.24
    
    Release-tracker: https://github.com/kubedb/CHANGELOG/pull/111
    Signed-off-by: 1gtm <1gtm@appscode.com>

--- catalog/scripts/perconaxtradb/import-images.sh
+++ catalog/scripts/perconaxtradb/import-images.sh
@@ -29,6 +29,6 @@ CMD="./crane"
 $CMD push --allow-nondistributable-artifacts --insecure images/appscode-images-percona-xtradb-cluster-5.7.44.tar $IMAGE_REGISTRY/appscode-images/percona-xtradb-cluster:5.7.44
 $CMD push --allow-nondistributable-artifacts --insecure images/appscode-images-percona-xtradb-cluster-8.0.40.tar $IMAGE_REGISTRY/appscode-images/percona-xtradb-cluster:8.0.40
 $CMD push --allow-nondistributable-artifacts --insecure images/appscode-images-percona-xtradb-cluster-8.4.3.tar $IMAGE_REGISTRY/appscode-images/percona-xtradb-cluster:8.4.3
-$CMD push --allow-nondistributable-artifacts --insecure images/kubedb-percona-xtradb-coordinator-v0.26.0-rc.1.tar $IMAGE_REGISTRY/kubedb/percona-xtradb-coordinator:v0.26.0-rc.1
+$CMD push --allow-nondistributable-artifacts --insecure images/kubedb-percona-xtradb-coordinator-v0.26.0.tar $IMAGE_REGISTRY/kubedb/percona-xtradb-coordinator:v0.26.0
 $CMD push --allow-nondistributable-artifacts --insecure images/kubedb-percona-xtradb-init-0.2.3.tar $IMAGE_REGISTRY/kubedb/percona-xtradb-init:0.2.3
 $CMD push --allow-nondistributable-artifacts --insecure images/prom-mysqld-exporter-v0.13.0.tar $IMAGE_REGISTRY/prom/mysqld-exporter:v0.13.0
