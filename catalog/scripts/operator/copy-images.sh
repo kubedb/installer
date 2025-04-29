0d6589353b9038e3cc4c5303a7278da80c6eff4f
SK Ali Arman <arman@appscode.com>
2025-04-29 18:55:09 +0600

    Update MySQL Init Image (#1667)
    Signed-off-by: SK Ali Arman <arman@appscode.com>

--- catalog/scripts/operator/copy-images.sh
+++ catalog/scripts/operator/copy-images.sh
@@ -46,12 +46,12 @@ $CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubedb/kubedb-crd-
 $CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubedb/kubedb-gitops:v0.1.0 $IMAGE_REGISTRY/kubedb/kubedb-gitops:v0.1.0
 $CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubedb/kubedb-kibana:v0.29.0 $IMAGE_REGISTRY/kubedb/kubedb-kibana:v0.29.0
 $CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubedb/kubedb-manifest-plugin:v0.16.0 $IMAGE_REGISTRY/kubedb/kubedb-manifest-plugin:v0.16.0
-$CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubedb/kubedb-ops-manager:v0.40.0 $IMAGE_REGISTRY/kubedb/kubedb-ops-manager:v0.40.0
-$CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubedb/kubedb-provisioner:v0.53.0 $IMAGE_REGISTRY/kubedb/kubedb-provisioner:v0.53.0
+$CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubedb/kubedb-ops-manager:v0.40.1 $IMAGE_REGISTRY/kubedb/kubedb-ops-manager:v0.40.1
+$CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubedb/kubedb-provisioner:v0.53.1 $IMAGE_REGISTRY/kubedb/kubedb-provisioner:v0.53.1
 $CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubedb/kubedb-schema-manager:v0.29.0 $IMAGE_REGISTRY/kubedb/kubedb-schema-manager:v0.29.0
 $CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubedb/kubedb-ui-server:v0.29.0 $IMAGE_REGISTRY/kubedb/kubedb-ui-server:v0.29.0
 $CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubedb/kubedb-verifier:v0.1.0 $IMAGE_REGISTRY/kubedb/kubedb-verifier:v0.1.0
-$CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubedb/kubedb-webhook-server:v0.29.0 $IMAGE_REGISTRY/kubedb/kubedb-webhook-server:v0.29.0
+$CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubedb/kubedb-webhook-server:v0.29.1 $IMAGE_REGISTRY/kubedb/kubedb-webhook-server:v0.29.1
 $CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubedb/mariadb-csi-snapshotter-plugin:v0.13.0 $IMAGE_REGISTRY/kubedb/mariadb-csi-snapshotter-plugin:v0.13.0
 $CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubedb/mariadb-restic-plugin:v0.11.0 $IMAGE_REGISTRY/kubedb/mariadb-restic-plugin:v0.11.0
 $CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubedb/mongodb-csi-snapshotter-plugin:v0.14.0 $IMAGE_REGISTRY/kubedb/mongodb-csi-snapshotter-plugin:v0.14.0
