1123c1892b95b6a2670db7a6b8a097b4f6db9e38
Sabnaj-42 <106393405+Sabnaj-42@users.noreply.github.com>
2025-12-08 20:02:55 +0600

    Add Db2 Version (#1904)
    Signed-off-by: obaydullahmhs <obaydullah@appscode.com>
    Co-authored-by: obaydullahmhs <obaydullah@appscode.com>
    Co-authored-by: Arnob kumar saha <arnob@appscode.com>

--- catalog/copy-images.sh
+++ catalog/copy-images.sh
@@ -327,6 +327,7 @@ $CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubedb/cassandra-m
 $CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubedb/clickhouse-init:24.4.1-v3 $IMAGE_REGISTRY/kubedb/clickhouse-init:24.4.1-v3
 $CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubedb/dashboard-restic-plugin:v0.1.0 $IMAGE_REGISTRY/kubedb/dashboard-restic-plugin:v0.1.0
 $CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubedb/dashboard-restic-plugin:v0.17.0 $IMAGE_REGISTRY/kubedb/dashboard-restic-plugin:v0.17.0
+$CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubedb/db2-coordinator:v0.1.0 $IMAGE_REGISTRY/kubedb/db2-coordinator:v0.1.0
 $CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubedb/druid-init:28.0.1 $IMAGE_REGISTRY/kubedb/druid-init:28.0.1
 $CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubedb/druid-init:30.0.1 $IMAGE_REGISTRY/kubedb/druid-init:30.0.1
 $CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubedb/druid-init:31.0.0 $IMAGE_REGISTRY/kubedb/druid-init:31.0.0
@@ -444,6 +445,7 @@ $CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubedb/zookeeper-i
 $CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubedb/zookeeper-restic-plugin:v0.15.0 $IMAGE_REGISTRY/kubedb/zookeeper-restic-plugin:v0.15.0
 $CMD cp --allow-nondistributable-artifacts --insecure hazelcast/hazelcast-enterprise:5.5.2 $IMAGE_REGISTRY/hazelcast/hazelcast-enterprise:5.5.2
 $CMD cp --allow-nondistributable-artifacts --insecure hazelcast/hazelcast-enterprise:5.5.6 $IMAGE_REGISTRY/hazelcast/hazelcast-enterprise:5.5.6
+$CMD cp --allow-nondistributable-artifacts --insecure ibmcom/db2:11.5.8.0 $IMAGE_REGISTRY/ibmcom/db2:11.5.8.0
 $CMD cp --allow-nondistributable-artifacts --insecure mariadb/maxscale:24.02.4 $IMAGE_REGISTRY/mariadb/maxscale:24.02.4
 $CMD cp --allow-nondistributable-artifacts --insecure mcr.microsoft.com/mssql/server:2022-CU12-ubuntu-22.04 $IMAGE_REGISTRY/mssql/server:2022-CU12-ubuntu-22.04
 $CMD cp --allow-nondistributable-artifacts --insecure mcr.microsoft.com/mssql/server:2022-CU14-ubuntu-22.04 $IMAGE_REGISTRY/mssql/server:2022-CU14-ubuntu-22.04
