b423856b0ff54b1d6308c00e2038fa798019e76d
Tamal Saha <tamal@appscode.com>
2025-12-09 11:43:26 +0600

    Prepare chart for redhat certification (#1955)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- catalog/scripts/operator/import-images.sh
+++ catalog/scripts/operator/import-images.sh
@@ -27,8 +27,8 @@ tar -zxvf $TARBALL
 CMD="./crane"
 
 $CMD push --allow-nondistributable-artifacts --insecure images/appscode-kubectl-nonroot-1.31.tar $IMAGE_REGISTRY/appscode/kubectl-nonroot:1.31
-$CMD push --allow-nondistributable-artifacts --insecure images/appscode-petset-v0.0.14.tar $IMAGE_REGISTRY/appscode/petset:v0.0.14
-$CMD push --allow-nondistributable-artifacts --insecure images/appscode-sidekick-v0.0.11.tar $IMAGE_REGISTRY/appscode/sidekick:v0.0.11
+$CMD push --allow-nondistributable-artifacts --insecure images/appscode-petset-v0.0.15.tar $IMAGE_REGISTRY/appscode/petset:v0.0.15
+$CMD push --allow-nondistributable-artifacts --insecure images/appscode-sidekick-v0.0.12.tar $IMAGE_REGISTRY/appscode/sidekick:v0.0.12
 $CMD push --allow-nondistributable-artifacts --insecure images/kubedb-cassandra-medusa-plugin-v0.6.0.tar $IMAGE_REGISTRY/kubedb/cassandra-medusa-plugin:v0.6.0
 $CMD push --allow-nondistributable-artifacts --insecure images/kubedb-dashboard-restic-plugin-v0.1.0.tar $IMAGE_REGISTRY/kubedb/dashboard-restic-plugin:v0.1.0
 $CMD push --allow-nondistributable-artifacts --insecure images/kubedb-dashboard-restic-plugin-v0.17.0.tar $IMAGE_REGISTRY/kubedb/dashboard-restic-plugin:v0.17.0
