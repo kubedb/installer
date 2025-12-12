43aa2e904e44c3ee4612810561512c5442c3b15b
Masood <masood@appscode.com>
2025-12-12 14:52:48 +0600

    templates fixed
    Signed-off-by: Masood <masood@appscode.com>
    
    # Conflicts:
    #	go.sum
    #	vendor/kubedb.dev/apimachinery/apis/kubedb/v1alpha2/openapi_generated.go
    #	vendor/kubedb.dev/apimachinery/apis/kubedb/v1alpha2/qdrant_helpers.go
    #	vendor/modules.txt

--- vendor/kubedb.dev/apimachinery/apis/kubedb/v1alpha2/zz_generated.deepcopy.go
+++ vendor/kubedb.dev/apimachinery/apis/kubedb/v1alpha2/zz_generated.deepcopy.go
@@ -6255,6 +6255,11 @@ func (in *QdrantSpec) DeepCopyInto(out *QdrantSpec) {
 		}
 	}
 	in.HealthChecker.DeepCopyInto(&out.HealthChecker)
+	if in.Monitor != nil {
+		in, out := &in.Monitor, &out.Monitor
+		*out = new(monitoringagentapiapiv1.AgentSpec)
+		(*in).DeepCopyInto(*out)
+	}
 	return
 }
 
