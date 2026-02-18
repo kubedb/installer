cf568cc0e933fcaae72e0d22dcf8558596276601
lgtm <1gtm@users.noreply.github.com>
2026-02-18 15:06:46 +0530

    Prepare for release v2026.2.16-rc.0 (#2100)
    * Prepare for release v2026.2.16-rc.0
    
    ProductLine: KubeDB
    
    Release: v2026.2.16-rc.0
    
    Release-tracker: https://github.com/kubedb/CHANGELOG/pull/125
    Signed-off-by: 1gtm <1gtm@appscode.com>
    
    * migrator fixes
    
    Signed-off-by: Tamal Saha <tamal@appscode.com>
    
    * Add missing images archs to skip list
    
    Signed-off-by: souravbiswassanto <saurov@appscode.com>
    
    ---------
    
    Signed-off-by: 1gtm <1gtm@appscode.com>
    Signed-off-by: Tamal Saha <tamal@appscode.com>
    Signed-off-by: souravbiswassanto <saurov@appscode.com>
    Co-authored-by: Tamal Saha <tamal@appscode.com>
    Co-authored-by: souravbiswassanto <saurov@appscode.com>

---
+++ apis/installer/v1alpha1/kubedb_migrator_types.go
@@ -0,0 +1,98 @@
+/*
+Copyright AppsCode Inc. and Contributors
+
+Licensed under the AppsCode Community License 1.0.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://github.com/appscode/licenses/raw/1.0.0/AppsCode-Community-1.0.0.md
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	core "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"kmodules.xyz/resource-metadata/apis/shared"
+)
+
+const (
+	ResourceKindKubedbMigrator = "KubedbMigrator"
+	ResourceKubedbMigrator     = "kubedbmigrator"
+	ResourceKubedbMigrators    = "kubedbmigrators"
+)
+
+// KubedbMigrator defines the schama for KubeDB Migrator installer.
+
+// +genclient
+// +genclient:skipVerbs=updateStatus
+// +k8s:openapi-gen=true
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// +kubebuilder:object:root=true
+// +kubebuilder:resource:path=kubedbmigrators,singular=kubedbmigrator,categories={kubeops,appscode}
+type KubedbMigrator struct {
+	metav1.TypeMeta   `json:",inline,omitempty"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+	Spec              KubedbMigratorSpec `json:"spec,omitempty"`
+}
+
+// KubedbMigratorSpec is the schema for Identity Server values file
+type KubedbMigratorSpec struct {
+	//+optional
+	NameOverride string `json:"nameOverride"`
+	//+optional
+	FullnameOverride string `json:"fullnameOverride"`
+	//+optional
+	RegistryFQDN string         `json:"registryFQDN"`
+	ReplicaCount int32          `json:"replicaCount"`
+	Image        ImageReference `json:"image"`
+	//+optional
+	ImagePullSecrets []string `json:"imagePullSecrets"`
+	//+optional
+	PodAnnotations map[string]string `json:"podAnnotations"`
+	//+optional
+	PodLabels map[string]string `json:"podLabels"`
+	// PodSecurityContext holds pod-level security attributes and common container settings.
+	// Optional: Defaults to empty.  See type description for default values of each field.
+	// +optional
+	PodSecurityContext *core.PodSecurityContext `json:"podSecurityContext"`
+	//+optional
+	SecurityContext *core.SecurityContext `json:"securityContext"`
+	//+optional
+	Resources core.ResourceRequirements `json:"resources"`
+	//+optional
+	NodeSelector map[string]string `json:"nodeSelector"`
+	// If specified, the pod's tolerations.
+	// +optional
+	Tolerations []core.Toleration `json:"tolerations"`
+	// If specified, the pod's scheduling constraints
+	// +optional
+	Affinity *core.Affinity `json:"affinity"`
+	// +optional
+	LivenessProbe *core.Probe `json:"livenessProbe"`
+	// +optional
+	ReadinessProbe *core.Probe        `json:"readinessProbe"`
+	Service        ServiceSpec        `json:"service"`
+	ServiceAccount ServiceAccountSpec `json:"serviceAccount"`
+	Volumes        []core.Volume      `json:"volumes"`
+	VolumeMounts   []core.VolumeMount `json:"volumeMounts"`
+	// +optional
+	Distro shared.DistroSpec `json:"distro"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// KubedbMigratorList is a list of KubedbMigrators
+type KubedbMigratorList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	// Items is a list of KubedbMigrator CRD objects
+	Items []KubedbMigrator `json:"items,omitempty"`
+}
