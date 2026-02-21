b0befb38175c6ba9036666db2f313449b0220499
1gtm <1gtm@appscode.com>
2026-02-21 01:49:48 +0000

    Prepare for release v2026.2.21-rc.1
    ProductLine: KubeDB
    
    Release: v2026.2.21-rc.1
    
    Release-tracker: https://github.com/kubedb/CHANGELOG/pull/126
    Signed-off-by: 1gtm <1gtm@appscode.com>

--- catalog/scripts/mssqlserver/import-images.sh
+++ catalog/scripts/mssqlserver/import-images.sh
@@ -26,10 +26,10 @@ tar -zxvf $TARBALL
 
 CMD="./crane"
 
-$CMD push --allow-nondistributable-artifacts --insecure images/kubedb-mssql-coordinator-v0.16.0-rc.0.tar $IMAGE_REGISTRY/kubedb/mssql-coordinator:v0.16.0-rc.0
+$CMD push --allow-nondistributable-artifacts --insecure images/kubedb-mssql-coordinator-v0.16.0-rc.1.tar $IMAGE_REGISTRY/kubedb/mssql-coordinator:v0.16.0-rc.1
 $CMD push --allow-nondistributable-artifacts --insecure images/kubedb-mssql-exporter-1.2.0.tar $IMAGE_REGISTRY/kubedb/mssql-exporter:1.2.0
 $CMD push --allow-nondistributable-artifacts --insecure images/kubedb-mssql-init-v2026.1.19.tar $IMAGE_REGISTRY/kubedb/mssql-init:v2026.1.19
-$CMD push --allow-nondistributable-artifacts --insecure images/kubedb-mssqlserver-archiver-v0.15.0-rc.0.tar $IMAGE_REGISTRY/kubedb/mssqlserver-archiver:v0.15.0-rc.0
+$CMD push --allow-nondistributable-artifacts --insecure images/kubedb-mssqlserver-archiver-v0.15.0-rc.1.tar $IMAGE_REGISTRY/kubedb/mssqlserver-archiver:v0.15.0-rc.1
 $CMD push --allow-nondistributable-artifacts --insecure images/mssql-server-2022-CU12-ubuntu-22.04.tar $IMAGE_REGISTRY/mssql/server:2022-CU12-ubuntu-22.04
 $CMD push --allow-nondistributable-artifacts --insecure images/mssql-server-2022-CU14-ubuntu-22.04.tar $IMAGE_REGISTRY/mssql/server:2022-CU14-ubuntu-22.04
 $CMD push --allow-nondistributable-artifacts --insecure images/mssql-server-2022-CU16-ubuntu-22.04.tar $IMAGE_REGISTRY/mssql/server:2022-CU16-ubuntu-22.04
