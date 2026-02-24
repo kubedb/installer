70b6047eca81b5c2368fc08d57f7ad4c96a7c2bb
Urmi Biswas <139246745+biswasurmi@users.noreply.github.com>
2026-02-24 12:40:46 +0600

    Add milvus new version (#2097)
    Signed-off-by: urmi <urmi@appscode.com>
    Co-authored-by: Saurov Chandra Biswas <48715910+souravbiswassanto@users.noreply.github.com>

--- catalog/import-into-k3s.sh
+++ catalog/import-into-k3s.sh
@@ -126,7 +126,9 @@ k3s ctr images import images/appscode-images-memcached-1.6.22-alpine.tar
 k3s ctr images import images/appscode-images-memcached-1.6.29-alpine.tar
 k3s ctr images import images/appscode-images-memcached-1.6.33-alpine.tar
 k3s ctr images import images/appscode-images-memcached_exporter-v0.14.3-ac.tar
+k3s ctr images import images/appscode-images-milvus-2.6.11.tar
 k3s ctr images import images/appscode-images-milvus-2.6.7.tar
+k3s ctr images import images/appscode-images-milvus-2.6.9.tar
 k3s ctr images import images/appscode-images-mongo-4.4.26.tar
 k3s ctr images import images/appscode-images-mongo-5.0.31.tar
 k3s ctr images import images/appscode-images-mongo-6.0.24.tar
