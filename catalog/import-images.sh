c759aaff8bb355335aa139bbeb9289f76f88f2ab
souravbiswassanto <sourav.cse4.bu@gmail.com>
2026-07-10 17:41:21 +0600

    Add postgres extension support for pgvector, pg_repack, postgis, pg_cron, pgaudit
    Signed-off-by: souravbiswassanto <sourav.cse4.bu@gmail.com>

--- catalog/import-images.sh
+++ catalog/import-images.sh
@@ -440,7 +440,7 @@ $CMD push --allow-nondistributable-artifacts --insecure images/kubedb-postgres-a
 $CMD push --allow-nondistributable-artifacts --insecure images/kubedb-postgres-archiver-v0.26.0_18.2-alpine.tar $IMAGE_REGISTRY/kubedb/postgres-archiver:v0.26.0_18.2-alpine
 $CMD push --allow-nondistributable-artifacts --insecure images/kubedb-postgres-archiver-v0.26.0_18.2-bookworm.tar $IMAGE_REGISTRY/kubedb/postgres-archiver:v0.26.0_18.2-bookworm
 $CMD push --allow-nondistributable-artifacts --insecure images/kubedb-postgres-csi-snapshotter-plugin-v0.26.0.tar $IMAGE_REGISTRY/kubedb/postgres-csi-snapshotter-plugin:v0.26.0
-$CMD push --allow-nondistributable-artifacts --insecure images/kubedb-postgres-init-0.19.0.tar $IMAGE_REGISTRY/kubedb/postgres-init:0.19.0
+$CMD push --allow-nondistributable-artifacts --insecure images/kubedb-postgres-init-0.19.0.tar $IMAGE_REGISTRY/kubedb/postgres-init:0.20.0
 $CMD push --allow-nondistributable-artifacts --insecure images/kubedb-postgres-restic-plugin-v0.28.0_16.4.tar $IMAGE_REGISTRY/kubedb/postgres-restic-plugin:v0.28.0_16.4
 $CMD push --allow-nondistributable-artifacts --insecure images/kubedb-provider-aws-v0.26.0.tar $IMAGE_REGISTRY/kubedb/provider-aws:v0.26.0
 $CMD push --allow-nondistributable-artifacts --insecure images/kubedb-provider-azure-v0.26.0.tar $IMAGE_REGISTRY/kubedb/provider-azure:v0.26.0
