f03fc80d6cf6d84d4b3514ed78850bfb6f4e443f
Arnob kumar saha <arnob@appscode.com>
2026-08-21 19:06:33 +0600

    Update the subcharts dependancies before the parent charts
    Signed-off-by: Arnob kumar saha <arnob@appscode.com>

--- hack/scripts/update-chart-dependencies.sh
+++ hack/scripts/update-chart-dependencies.sh
@@ -18,6 +18,7 @@ set -e
 
 helm repo add appscode https://charts.appscode.com/stable/ || true
 
-helm dependency update charts/kubedb
-helm dependency update charts/kubedb-opscenter
 helm dependency update charts/kubedb-autoscaler
+helm dependency update charts/kubedb
+helm dependency update charts/kubedb-certified
+helm dependency update charts/kubedb-opscenter
