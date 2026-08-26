7ea6575e3aeee294a5daaaa824746c48d207710f
Tamal Saha <tamal@appscode.com>
2026-08-26 14:01:14 +0600

    Fix script
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- hack/scripts/update-chart-dependencies.sh
+++ hack/scripts/update-chart-dependencies.sh
@@ -20,5 +20,4 @@ helm repo add appscode https://charts.appscode.com/stable/ || true
 
 helm dependency update charts/kubedb-autoscaler
 helm dependency update charts/kubedb
-helm dependency update charts/kubedb-certified
 helm dependency update charts/kubedb-opscenter
