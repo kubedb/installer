78e0c79b148c85742f36095b7c90db89f346f687
Sajjad Sadi <sajjadsadi@Sajjads-MacBook-Air.local>
2026-09-14 11:33:09 +0600

    Remove extra code
    Signed-off-by: Sajjad Sadi <sajjadsadi@Sajjads-MacBook-Air.local>

--- catalog/copy-images.sh
+++ catalog/copy-images.sh
@@ -489,7 +489,6 @@ $CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubedb/mysql-resti
 $CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubedb/mysql-restic-plugin:v0.30.0-rc.2_9.0.1 $IMAGE_REGISTRY/kubedb/mysql-restic-plugin:v0.30.0-rc.2_9.0.1
 $CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubedb/mysql-router-init:v0.45.0-rc.2 $IMAGE_REGISTRY/kubedb/mysql-router-init:v0.45.0-rc.2
 $CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubedb/mysqld-exporter:v0.18.0 $IMAGE_REGISTRY/kubedb/mysqld-exporter:v0.18.0
-$CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubedb/neo4j-backup-plugin:v0.2.0 $IMAGE_REGISTRY/kubedb/neo4j-backup-plugin:v0.2.0
 $CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubedb/oracle-backup-plugin:v0.1.0 $IMAGE_REGISTRY/kubedb/oracle-backup-plugin:v0.1.0
 $CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubedb/oracle-coordinator:v0.12.0 $IMAGE_REGISTRY/kubedb/oracle-coordinator:v0.12.0
 $CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubedb/neo4j-backup-plugin:v0.3.0-rc.2 $IMAGE_REGISTRY/kubedb/neo4j-backup-plugin:v0.3.0-rc.2
