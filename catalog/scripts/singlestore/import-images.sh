f048822dc1ade87402faa685980c8d885b88eb5c
sabnaj-42 <sabnaj@appscode.com>
2026-09-16 16:08:09 +0600

    update deps
    Signed-off-by: sabnaj-42 <sabnaj@appscode.com>

--- catalog/scripts/singlestore/import-images.sh
+++ catalog/scripts/singlestore/import-images.sh
@@ -40,4 +40,3 @@ $CMD push --allow-nondistributable-artifacts --insecure images/kubedb-singlestor
 $CMD push --allow-nondistributable-artifacts --insecure images/kubedb-singlestore-init-8.1-v2.tar $IMAGE_REGISTRY/kubedb/singlestore-init:8.1-v2
 $CMD push --allow-nondistributable-artifacts --insecure images/kubedb-singlestore-init-8.5-v2.tar $IMAGE_REGISTRY/kubedb/singlestore-init:8.5-v2
 $CMD push --allow-nondistributable-artifacts --insecure images/kubedb-singlestore-init-8.7.10-v1.tar $IMAGE_REGISTRY/kubedb/singlestore-init:8.7.10-v1
-$CMD push --allow-nondistributable-artifacts --insecure images/git-sync-git-sync-v4.4.2.tar $IMAGE_REGISTRY/git-sync/git-sync:v4.4.2
